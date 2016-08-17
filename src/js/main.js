b9478ae4630f1ba6bc7ad73b677999e502924f70
Felipe Pontes <felipemfpontes@gmail.com>
2016-08-17 09:02:35 -0300

    Update contents

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,6 @@ Reveal.initialize({
 	// Optional reveal.js plugins
 	dependencies: [
 		{ src: '/js/vendor/classList.js', condition: function() { return !document.body.classList; } },
-		{ src: 'js/vendor/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-		{ src: 'js/vendor/notes/notes.js', async: true }
+		{ src: 'js/vendor/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } }
 	]
 });
